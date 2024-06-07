640132ac294b41fe5390cc10735dd4a1063d4398
Darien Campbell <dacman9@gmail.com>
2024-06-07 17:07:11 -0400

    Increased planet and star scales

--- game/FreeFlyGameEngine.h
+++ game/FreeFlyGameEngine.h
@@ -17,7 +17,7 @@ class FreeFlyGameEngine: public IGameEngine {
 private:
     static const inline float SCALE_MULT = 1.001;
     static const inline glm::vec3 SCALE_POS = {-10000.0f, 0.0f, 0.0f};
-    static const inline float SCALE_BORDER_RADIUS = 1.0f;
+    static const inline float SCALE_BORDER_RADIUS = 1000.0f;
     static const inline bool ORIENT_TO_PLANET = false;
 
     float camSpeed;
