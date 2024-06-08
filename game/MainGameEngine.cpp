bca7994398463af342127b1ef1c0c17f2bc2c841
Darien Campbell <dacman9@gmail.com>
2024-06-07 20:33:19 -0400

    Added basic support for panning the camera in the main engine

--- game/MainGameEngine.cpp
+++ game/MainGameEngine.cpp
@@ -21,7 +21,7 @@ void MainGameEngine::fixCamera(GameState::CameraState &camera, Controls &control
     glm::vec3 desiredDir = ship.modelState.dir;
     desiredDir = glm::rotate(glm::mat4(1.0f), controls.cameraPanDir.x * (pi * 0.5f), ship.modelState.up) * glm::vec4(desiredDir, 0.0f);
     glm::vec3 right = glm::normalize(glm::cross(desiredDir, camera.up));
-    desiredDir = glm::rotate(glm::mat4(1.0f), controls.cameraPanDir.y * (pi * -0.5f), right) * glm::vec4(desiredDir, 0.0f);
+    desiredDir = glm::rotate(glm::mat4(1.0f), controls.cameraPanDir.y * (pi * -0.25f), right) * glm::vec4(desiredDir, 0.0f);
 
     camera.dir = glm::normalize(glm::normalize(desiredDir) * 0.02f + camera.dir);
     camera.pos = ship.modelState.pos + ship.modelState.up * 1.5f - 8.0f * camera.dir;
